9847b0d7c73ea29968b95e6e63500c20a994cbb6
Guillaume Rousse <guillomovitch@gmail.com>
2006-01-26 21:30:37 +0000

    keep authors in a centralized place add copyright statement in each file

--- lib/Youri/Media/Base.pm
+++ lib/Youri/Media/Base.pm
@@ -263,6 +263,12 @@ The following methods have to be implemented:
 
 =back
 
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2002-2006, YOURI project
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
 =cut
 
 1;
--- lib/Youri/Media/URPM.pm
+++ lib/Youri/Media/URPM.pm
@@ -254,4 +254,12 @@ sub _get_file {
     }
 }
 
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2002-2006, YOURI project
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
 1;
